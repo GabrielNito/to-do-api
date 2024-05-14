f8a47855bad84894efbbbc3ec8a3ead9e82e21cc
Gabriel Nito <nito.gabriel22@gmail.com>
2024-05-14 09:23:36 -0300

    test

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,18 @@
-import express from "express";
-import dotenv from "dotenv";
-import dataBase from "./database/ormconfig";
-import routes from "./routes";
-import cors from "cors";
+import express from 'express'
+import dotenv from 'dotenv'
+import dataBase from './database/ormconfig'
+import routes from './routes'
+import cors from 'cors'
 
-export function DB() {
-  dotenv.config();
-  const app = express();
-  const port = process.env.PORT || 3001;
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 3001
 
-  app.use(cors());
-  app.use(express.json());
-  app.use(routes);
+app.use(cors()) // habilita o cors
+app.use(express.json()) // habilita o express para receber dados no formato json
+app.use(routes) // habilita as rotas
 
-  app.listen(port, () => {
-    console.log(`Servidor executando na porta ${port}`);
-    console.log(
-      `Banco de dados`,
-      dataBase.isInitialized ? "inicializado" : "não inicializado"
-    );
-  });
-}
-
-// dotenv.config()
-// const app = express()
-// const port = process.env.PORT || 3001
-
-// app.use(cors()) // habilita o cors
-// app.use(express.json()) // habilita o express para receber dados no formato json
-// app.use(routes) // habilita as rotas
-
-// app.listen(port, () => {
-//   console.log(`Servidor executando na porta ${port}`)
-//   console.log(`Banco de dados`, dataBase.isInitialized ? 'inicializado' : 'não inicializado')
-// })
+app.listen(port, () => {
+  console.log(`Servidor executando na porta ${port}`)
+  console.log(`Banco de dados`, dataBase.isInitialized ? 'inicializado' : 'não inicializado')
+})
